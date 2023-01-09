d4b4d12d8fb7d6a9e4aa7457101196a98d6a02e5
Humberto <hdkaratechop@gmail.com>
2023-01-09 15:05:19 -0800

    run migrations for post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_09_222937) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_09_230200) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
