25da6db426e96d8b8224013fa6c82ef3a428bd63
Humberto <hdkaratechop@gmail.com>
2023-01-09 18:04:56 -0800

    create users controller and new user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+      end
+    
+      def create
+            @user = User.new(user_params)
+          
+            if @user.save
+              redirect_to new_user_path
+            else
+              render :new
+    
+            end
+          end
+    
+      def edit
+        @user = User.find(params[:id])
+      end
+    
+      def update
+        @user = User.find(params[:id])
+    
+        if @user.update(user_params)
+          redirect_to @user
+        else
+          render :edit
+        end
+      end
+    
+    
+      private
+      # returns the hash containing the params we need
+      # to create or update a post
+      def user_params
+        params.require(:users).permit(:username,:email,:password)
+      end
 end
